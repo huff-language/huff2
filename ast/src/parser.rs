1b939b15943f72b0c802f3ed740c29175f3f69c9
lmittmann <lmittmann@users.noreply.github.com>
2024-10-03 08:21:21 +0200

    fixed whitespace and comment handling

--- ast/src/parser.rs
+++ ast/src/parser.rs
@@ -8,24 +8,43 @@ pub fn parse(src: &str) -> Result<ast::Root, ParseError<usize, Token<'_>, &str>>
 
 #[cfg(test)]
 mod tests {
-    use crate::HuffDefinition;
-
     use super::*;
-    use alloy_primitives::uint;
+    use alloy_primitives::{uint, U256};
+
+    #[test]
+    fn word() {
+        assert_eq!(grammar::WordParser::new().parse("0x0"), Ok(U256::ZERO));
+        assert_eq!(grammar::WordParser::new().parse("0x1"), Ok(uint!(1_U256)));
+        assert_eq!(
+            grammar::WordParser::new()
+                .parse("0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff"),
+            Ok(U256::MAX)
+        );
+        assert_eq!(
+            grammar::WordParser::new()
+                .parse("0x10000000000000000000000000000000000000000000000000000000000000000"),
+            Ok(U256::MAX + uint!(1_U256))
+        );
+    }
 
     #[test]
     fn constant() {
-        assert!(grammar::ConstantParser::new()
-            .parse("#define constant TEST =    1")
-            .is_ok());
+        let want = Ok(ast::HuffDefinition::Constant {
+            name: "TEST",
+            value: uint!(1_U256),
+        });
+        assert_eq!(
+            grammar::ConstantParser::new().parse("#define constant TEST = 0x1"),
+            want
+        );
+        assert_eq!(
+            grammar::ConstantParser::new().parse(" #define constant TEST = 0x1 "),
+            want
+        );
         assert_eq!(
             grammar::ConstantParser::new()
-                .parse("#define constant TEST = 1")
-                .unwrap(),
-            HuffDefinition::Constant {
-                name: "TEST",
-                value: uint!(1_U256)
-            }
+                .parse("#define constant TEST /* comment */ = 0x1 // comment"),
+            want
         );
     }
 }
