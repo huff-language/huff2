5dc947382242a7af73797144f2c709b2f25d7557
lmittmann <lmittmann@users.noreply.github.com>
2024-10-11 23:15:47 +0200

    added parser (wip)

--- crates/ast/src/lib.rs
+++ crates/ast/src/lib.rs
@@ -2,14 +2,10 @@ mod error;
 mod lexer;
 mod parser;
 
-use chumsky::span::SimpleSpan;
-pub use error::Error;
-pub use parser::parse;
-
-lalrpop_util::lalrpop_mod!(grammar);
-
 use alloy_dyn_abi::DynSolType;
 use alloy_primitives::U256;
+use chumsky::span::SimpleSpan;
+pub use error::Error;
 use evm_glue::opcodes::Opcode;
 
 pub struct Root<'src>(pub Box<[Definition<'src>]>);
@@ -17,9 +13,15 @@ pub struct Root<'src>(pub Box<[Definition<'src>]>);
 #[derive(Debug, PartialEq, Eq)]
 pub enum Definition<'src> {
     Macro(Macro<'src>),
-    Constant { name: &'src str, value: U256 },
+    Constant {
+        name: Spanned<&'src str>,
+        value: U256,
+    },
     Jumptable(Jumptable<'src>),
-    Codetable { name: &'src str, data: Box<[u8]> },
+    Codetable {
+        name: Spanned<&'src str>,
+        data: Box<[u8]>,
+    },
     SolFunction(SolFunction<'src>),
     SolEvent(SolEvent<'src>),
     SolError(SolError<'src>),
@@ -27,40 +29,40 @@ pub enum Definition<'src> {
 
 #[derive(Debug, PartialEq, Eq)]
 pub struct Macro<'src> {
-    pub name: &'src str,
-    pub args: Box<[&'src str]>,
-    pub takes_returns: Option<(usize, usize)>,
+    pub name: Spanned<&'src str>,
+    pub args: Box<[Spanned<&'src str>]>,
+    pub takes_returns: Option<(Spanned<usize>, Spanned<usize>)>,
     pub body: Box<[MacroStatement<'src>]>,
 }
 
 #[derive(Debug, PartialEq, Eq)]
 pub enum MacroStatement<'src> {
-    LabelDefinition(&'src str),
+    LabelDefinition(Spanned<&'src str>),
     Instruction(Instruction<'src>),
     Invoke(Invoke<'src>),
 }
 
 #[derive(Debug, PartialEq, Eq)]
 pub enum Instruction<'src> {
-    Op(Opcode),
-    LabelReference(&'src str),
-    MacroArgReference(&'src str),
-    ConstantReference(&'src str),
+    Op(Spanned<Opcode>),
+    LabelReference(Spanned<&'src str>),
+    MacroArgReference(Spanned<&'src str>),
+    ConstantReference(Spanned<&'src str>),
 }
 
 #[derive(Debug, PartialEq, Eq)]
 pub enum Invoke<'src> {
     Macro {
-        name: &'src str,
+        name: Spanned<&'src str>,
         args: Box<[Instruction<'src>]>,
     },
-    BuiltinTableStart(&'src str),
-    BuiltinTableSize(&'src str),
-    BuiltinCodeSize(&'src str),
-    BuiltinCodeOffset(&'src str),
-    BuiltinFuncSig(&'src str),
-    BuiltinEventHash(&'src str),
-    BuiltinError(&'src str),
+    BuiltinTableStart(Spanned<&'src str>),
+    BuiltinTableSize(Spanned<&'src str>),
+    BuiltinCodeSize(Spanned<&'src str>),
+    BuiltinCodeOffset(Spanned<&'src str>),
+    BuiltinFuncSig(Spanned<&'src str>),
+    BuiltinEventHash(Spanned<&'src str>),
+    BuiltinError(Spanned<&'src str>),
 }
 
 #[derive(Debug, PartialEq, Eq)]
@@ -72,20 +74,20 @@ pub struct Jumptable<'src> {
 
 #[derive(Debug, PartialEq, Eq)]
 pub struct SolFunction<'src> {
-    pub name: &'src str,
+    pub name: Spanned<&'src str>,
     pub args: Box<[DynSolType]>,
     pub rets: Box<[DynSolType]>,
 }
 
 #[derive(Debug, PartialEq, Eq)]
 pub struct SolEvent<'src> {
-    pub name: &'src str,
+    pub name: Spanned<&'src str>,
     pub args: Box<[DynSolType]>,
 }
 
 #[derive(Debug, PartialEq, Eq)]
 pub struct SolError<'src> {
-    pub name: &'src str,
+    pub name: Spanned<&'src str>,
     pub args: Box<[DynSolType]>,
 }
 
